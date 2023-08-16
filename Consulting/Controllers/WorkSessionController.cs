eb488c814c79d889a4722e0d0d36b224eb19fdfe
MengKang <mmk070330@gmail.com>
2023-08-16 10:05:13 -0400

    Add project files.

---
+++ Consulting/Controllers/WorkSessionController.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Consulting.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Consulting.Controllers
+{
+    public class WorkSessionController : Controller
+    {
+        private readonly ConsultingContext _context;
+
+        public WorkSessionController(ConsultingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WorkSession
+        public async Task<IActionResult> Index(string ContractID)
+        {
+            if (!string.IsNullOrEmpty(ContractID))
+            {
+                Response.Cookies.Append("ContractID", ContractID);
+                HttpContext.Session.SetString("ContractID", ContractID);
+            }
+            else if (Request.Query["ContractID"].Any())
+            {
+                ContractID = Request.Query["ContractID"].ToString();
+                Response.Cookies.Append("ContractID", ContractID);
+                HttpContext.Session.SetString("ContractID", ContractID);
+            }
+            else if (Request.Cookies["ContractID"] != null)
+            {
+                ContractID = Request.Cookies["ContractID"].ToString();
+            }
+            else if (HttpContext.Session.GetString("ContractID")!= null)
+            {
+                ContractID = HttpContext.Session.GetString("ContractID");
+            }
+            else
+            {
+                TempData["message"] = "Please select Contract";
+                return RedirectToAction("Index", "Contract");
+            }
+
+            var consultingContext = _context.WorkSession.Include(w => w.Consultant).Include(w => w.Contract)
+                                    .Where(a=>a.ContractId == Convert.ToInt32(ContractID));
+
+            var workSessions = consultingContext.ToList();
+            ViewBag.totalhours = workSessions.Sum(a=>a.HoursWorked);
+            ViewBag.totalcost = workSessions.Sum(a=>a.HoursWorked * a.HourlyRate);
+
+            return View(await consultingContext.ToListAsync());
+        }
+
+        // GET: WorkSession/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.WorkSession == null)
+            {
+                return NotFound();
+            }
+
+            var workSession = await _context.WorkSession
+                .Include(w => w.Consultant)
+                .Include(w => w.Contract)
+                .FirstOrDefaultAsync(m => m.WorkSessionId == id);
+            if (workSession == null)
+            {
+                return NotFound();
+            }
+
+            return View(workSession);
+        }
+
+        // GET: WorkSession/Create
+        public IActionResult Create()
+        {
+            string contract = string.Empty;
+            if (Request.Cookies["ContractID"] != null)
+            {
+                contract = Request.Cookies["ContractID"].ToString();
+            }
+            else if(HttpContext.Session.GetString("ContractID") != null)
+            {
+                contract = HttpContext.Session.GetString("ContractID");
+            }
+
+            ViewBag.CID = contract;
+
+            ViewData["ConsultantId"] = new SelectList(_context.Consultant, "ConsultantId", "FirstName");
+            ViewData["ContractId"] = new SelectList(_context.Contract, "ContractId", "Name");
+            return View();
+        }
+
+        // POST: WorkSession/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WorkSessionId,ContractId,DateWorked,ConsultantId,HoursWorked,WorkDescription,HourlyRate,ProvincialTax,TotalChargeBeforeTax")] WorkSession workSession)
+        {
+            try
+            {
+                string contract = string.Empty;
+                if (Request.Cookies["ContractID"] != null)
+                {
+                    contract = Request.Cookies["ContractID"].ToString();
+                }
+                else if (HttpContext.Session.GetString("ContractID") != null)
+                {
+                    contract = HttpContext.Session.GetString("ContractID");
+                }
+                ViewBag.CID = contract;
+
+                if (ModelState.IsValid)
+                {
+                    _context.Add(workSession);
+                    await _context.SaveChangesAsync();
+                    TempData["message"] = "Created Successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", ex.GetBaseException().Message);
+                TempData["message"] = ex.GetBaseException().Message;
+            }
+            
+            ViewData["ConsultantId"] = new SelectList(_context.Consultant, "ConsultantId", "FirstName", workSession.ConsultantId);
+            ViewData["ContractId"] = new SelectList(_context.Contract, "ContractId", "Name", workSession.ContractId);
+            return View(workSession);
+        }
+
+        // GET: WorkSession/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.WorkSession == null)
+            {
+                return NotFound();
+            }
+
+            var workSession = await _context.WorkSession.FindAsync(id);
+            if (workSession == null)
+            {
+                return NotFound();
+            }
+            ViewData["ConsultantId"] = new SelectList(_context.Consultant, "ConsultantId", "FirstName", workSession.ConsultantId);
+            ViewData["ContractId"] = new SelectList(_context.Contract, "ContractId", "Name", workSession.ContractId);
+            return View(workSession);
+        }
+
+        // POST: WorkSession/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WorkSessionId,ContractId,DateWorked,ConsultantId,HoursWorked,WorkDescription,HourlyRate,ProvincialTax,TotalChargeBeforeTax")] WorkSession workSession)
+        {
+            if (id != workSession.WorkSessionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workSession);
+                    await _context.SaveChangesAsync();
+                    TempData["message"] = "Edit was successful";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkSessionExists(workSession.WorkSessionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.GetBaseException().Message);
+                    TempData["message"] = ex.GetBaseException().Message;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ConsultantId"] = new SelectList(_context.Consultant, "ConsultantId", "FirstName", workSession.ConsultantId);
+            ViewData["ContractId"] = new SelectList(_context.Contract, "ContractId", "Name", workSession.ContractId);
+            return View(workSession);
+        }
+
+        // GET: WorkSession/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.WorkSession == null)
+            {
+                return NotFound();
+            }
+
+            var workSession = await _context.WorkSession
+                .Include(w => w.Consultant)
+                .Include(w => w.Contract)
+                .FirstOrDefaultAsync(m => m.WorkSessionId == id);
+            if (workSession == null)
+            {
+                return NotFound();
+            }
+
+            return View(workSession);
+        }
+
+        // POST: WorkSession/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                if (_context.WorkSession == null)
+                {
+                    return Problem("Entity set 'ConsultingContext.WorkSession'  is null.");
+                }
+                var workSession = await _context.WorkSession.FindAsync(id);
+                if (workSession != null)
+                {
+                    _context.WorkSession.Remove(workSession);
+                }
+            
+                await _context.SaveChangesAsync();
+                TempData["message"] = "Delete was Successful";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.GetBaseException().Message);
+                TempData["message"] = ex.GetBaseException().Message;
+            }
+            return View("Delete");
+           
+        }
+
+        private bool WorkSessionExists(int id)
+        {
+          return _context.WorkSession.Any(e => e.WorkSessionId == id);
+        }
+    }
+}
---
+++ Consulting/Models/Metadata/WorkSessionMetaData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Consulting.Models
+{
+    [ModelMetadataType(typeof(WorkSessionMetaData))]
+
+    public partial class WorkSession : IValidatableObject
+    {
+        ConsultingContext _context = new ConsultingContext();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var contract = _context.Contract.Where(a=>a.ContractId == ContractId).FirstOrDefault();
+            if(contract != null)
+            {
+                if (contract.Closed)
+                    yield return new ValidationResult("Contract is closed", new[] { "ContractId" });
+            }
+            else
+                yield return new ValidationResult("Contract is closed", new[] { "ContractId" });
+            var consultant = _context.Consultant.Where(a=>a.ConsultantId == ConsultantId).FirstOrDefault();
+            if(consultant == null)
+                yield return new ValidationResult("Consutant ID is not correct", new[] { "ContractId" });
+
+            if(DateWorked != null)
+            {
+                if (DateWorked > DateTime.Now)
+                    yield return new ValidationResult("DateWorked cannot be in future", new[] { "DateWorked" });
+
+                if (DateWorked < contract.StartDate)
+                    yield return new ValidationResult("DateWorked cannot be before contract start date", new[] { "DateWorked" });
+
+            }
+
+            if(HoursWorked <=0)
+                yield return new ValidationResult("HoursWorked cannot be less than zero", new[] { "HoursWorked" });
+
+            var totalhoursinDatabase = _context.WorkSession
+                .Where(a => a.ConsultantId == ConsultantId && a.DateWorked == DateWorked).Sum(a => a.HoursWorked);
+
+            if(totalhoursinDatabase + HoursWorked >24)
+                yield return new ValidationResult("HoursWorked cannot be greater than 24", new[] { "HoursWorked" });
+
+            if(WorkSessionId == 0)
+            {
+                HourlyRate = consultant.HourlyRate;
+            }
+            else
+            {
+                if(HourlyRate <=0)
+                {
+                    yield return new ValidationResult("Hourly Rate cannot be less or equal to 0", new[] { "HourlyRate" });
+
+                }
+                if(HourlyRate *1.5 > Consultant.HourlyRate)
+                {
+                    yield return new ValidationResult("Hourly Rate cannot be greater than 1.5 times than the consultant hourly rate", new[] { "HourlyRate" });
+
+                }
+
+                TotalChargeBeforeTax = HourlyRate * HoursWorked;
+
+                var customer = _context.Customer.Where(a=>a.CustomerId == contract.CustomerId).FirstOrDefault();
+                var province = _context.Province.Where(a=>a.ProvinceCode == customer.ProvinceCode).FirstOrDefault();
+
+                ProvincialTax = TotalChargeBeforeTax * province.ProvincialTax;
+
+            }
+        }
+        public class WorkSessionMetaData
+        {
+            public int WorkSessionId { get; set; }
+            public int ContractId { get; set; }
+            public DateTime DateWorked { get; set; }
+            public int ConsultantId { get; set; }
+            public double HoursWorked { get; set; }
+            public string WorkDescription { get; set; }
+            public double HourlyRate { get; set; }
+            public double ProvincialTax { get; set; }
+            public double TotalChargeBeforeTax { get; set; }
+        }
+    }
+   
+}
